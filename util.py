5c6fe66d14a097c681b4fad09aa4e210782ed152
y19941115mx <1960892068@qq.com>
2018-10-01 10:06:58 +0800

    提交代码

--- util.py
+++ util.py
@@ -15,20 +15,20 @@ X-Requested-With: XMLHttpRequest
 """
 
 
-data_url = 'https://mp.weixin.qq.com/mp/getappmsgext?f=json&mock=&uin=MzQ3NDg1MTY3Nw%253D%253D&key=fbdef8d530cc7a1be3bbe6b5b440b65e3c6762a4c89eeb49ad7d0a090b4326ca34ae5137384525d46b7dbd3c967fe60f0a2f2b5de5023aba7372bb7d8567bcd320ee054fde7c54ac6f98528a9d290ec9&pass_ticket=ukQHWJ7lVsRjKvhVCFRFIf0kuuU4SSvl4C15JxNtf3MxQy%25252BtQfCSU%25252BGnOj0U4X6H&wxtoken=777&devicetype=iMac%26nbsp%3BMacBookAir5%2C2%26nbsp%3BOSX%26nbsp%3BOSX%26nbsp%3B10.13.5%26nbsp%3Bbuild(17F77)&clientversion=12031110&appmsg_token=976_CtcE3I0eDoBX%252F9xxjOJ3FB6dXlVWJfY7Om7_5WyQ27fvwdsy5FvxlusuDlzO__LMVAgWqtv8wRJOX14G&x5=0&f=json'
+data_url = 'https://mp.weixin.qq.com/mp/getappmsgext?f=json&mock=&uin=MzQ3NDg1MTY3Nw%253D%253D&key=b9e0608486d8e297facd738aaee5a179034621139f84ed2d02462edb5702fcfdb074c200f8bb889dc56618a17709260895fc3b881342477832aa75ff6b187146233f4591aaa2fa756566141cc90f727d&pass_ticket=ukQHWJ7lVsRjKvhVCFRFIf0kuuU4SSvl4C15JxNtf3MxQy%25252BtQfCSU%25252BGnOj0U4X6H&wxtoken=777&devicetype=iMac%26nbsp%3BMacBookAir5%2C2%26nbsp%3BOSX%26nbsp%3BOSX%26nbsp%3B10.13.5%26nbsp%3Bbuild(17F77)&clientversion=12031110&appmsg_token=976_dPgcvvGqczfCboTojOJ3FB6dXlVWJfY7Om7_5evQUlkXnHhR9tNIyWMVJctHfSchTuAFmqr3nxMA0Mo_&x5=0&f=json'
 data_headers = """
 Host: mp.weixin.qq.com
 Accept: */*
-X-Requested-Withz: XMLHttpRequest
+X-Requested-With: XMLHttpRequest
 Accept-Language: zh-cn
 Accept-Encoding: br, gzip, deflate
 Content-Type: application/x-www-form-urlencoded; charset=UTF-8
 Origin: https://mp.weixin.qq.com
 User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 MicroMessenger/6.5.2.501 NetType/WIFI WindowsWechat
 Connection: keep-alive
-Referer: https://mp.weixin.qq.com/s?__biz=MzI3NjA0ODE1OQ==&mid=2651829695&idx=1&sn=0cbcac3ee74d1453b0a23a1af59a3a75&chksm=f08065cfc7f7ecd9f0b789c6b537d696a1881e5eac5fbb23ecd29691e411d11f0c58258a3c76&scene=0&key=fbdef8d530cc7a1be3bbe6b5b440b65e3c6762a4c89eeb49ad7d0a090b4326ca34ae5137384525d46b7dbd3c967fe60f0a2f2b5de5023aba7372bb7d8567bcd320ee054fde7c54ac6f98528a9d290ec9&ascene=0&uin=MzQ3NDg1MTY3Nw%3D%3D&devicetype=iMac+MacBookAir5%2C2+OSX+OSX+10.13.5+build(17F77)&version=12031110&nettype=WIFI&lang=zh_CN&fontScale=100&pass_ticket=ukQHWJ7lVsRjKvhVCFRFIf0kuuU4SSvl4C15JxNtf3MxQy%2BtQfCSU%2BGnOj0U4X6H
-Content-Length: 750
-Cookie: devicetype=iMacMacBookAir52OSXOSX10.13.5build(17F77); lang=zh_CN; pass_ticket=ukQHWJ7lVsRjKvhVCFRFIf0kuuU4SSvl4C15JxNtf3MxQy+tQfCSU+GnOj0U4X6H; rewardsn=; version=12031110; wap_sid2=CN2O+PgMElxpUldMdEVoTmlSX3dNcjZUTEtKMXhCd2RDLWY3eHR3R1cxby05Q0J1a3prTl9DYUdfUm8zRmhqQllGbGVYY2dQREx1QXRGckRxSWMyZjlQX1VJZWxidEFEQUFBfjCmzMXdBTgNQAE=; wxtokenkey=777; wxuin=3474851677
+Referer: https://mp.weixin.qq.com/s?__biz=MzI3NjA0ODE1OQ==&mid=2651829695&idx=1&sn=0cbcac3ee74d1453b0a23a1af59a3a75&chksm=f08065cfc7f7ecd9f0b789c6b537d696a1881e5eac5fbb23ecd29691e411d11f0c58258a3c76&scene=0&key=b9e0608486d8e297facd738aaee5a179034621139f84ed2d02462edb5702fcfdb074c200f8bb889dc56618a17709260895fc3b881342477832aa75ff6b187146233f4591aaa2fa756566141cc90f727d&ascene=0&uin=MzQ3NDg1MTY3Nw%3D%3D&devicetype=iMac+MacBookAir5%2C2+OSX+OSX+10.13.5+build(17F77)&version=12031110&nettype=WIFI&lang=zh_CN&fontScale=100&pass_ticket=ukQHWJ7lVsRjKvhVCFRFIf0kuuU4SSvl4C15JxNtf3MxQy%2BtQfCSU%2BGnOj0U4X6H
+Content-Length: 752
+Cookie: devicetype=iMacMacBookAir52OSXOSX10.13.5build(17F77); lang=zh_CN; pass_ticket=ukQHWJ7lVsRjKvhVCFRFIf0kuuU4SSvl4C15JxNtf3MxQy+tQfCSU+GnOj0U4X6H; rewardsn=; version=12031110; wap_sid2=CN2O+PgMElxUM180a0dRck91ZnlLSEd1UzVqeUlRMEdnYmRTU05VdzZCV2t4MXZHV1psTlBYWWIzZXFFZXFVaFNUZ3VMNWxNeHhlanh4b19SOV9yNG51T3VCNnpkOUFEQUFBfjCY3cXdBTgNQAE=; wxtokenkey=777; wxuin=3474851677
 """
 
 import html
